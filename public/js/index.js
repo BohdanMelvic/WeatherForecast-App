5ed86dd18aa8d380bbd1aa7f3acad5508d9b45aa
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-10-08 12:28:35 +0300

    setup to heroku

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     massage1.textContent = 'Loading...';
     massage2.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             massage1.textContent = (data.error);
